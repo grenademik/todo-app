a8f71a099b3f1aa4e114eb89480f6ab2bd314407
grenademik <grenademik@gmail.com>
2022-11-17 00:29:58 +0545

    Latest

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
 function App() {
+  const AppName ='Naya Naam';
+  const [list, setlist] = useState([]);
+
+  const addedtodo = (task) => {
+    setlist((prev) =>[...prev, task])
+  }
+
+  const clearalltask = () => {
+    setlist([]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='todo-card'>
+    <TodoTitle title={AppName}/>
+    <AddTask addedTodo={addedtodo}/>
+    <ShowTask listOfTask={list}/>
+    <ClearTask cleartasks={clearalltask}/>
     </div>
   );
 }
+
+function TodoTitle({title}){
+  return(
+    <div>
+      <h1>{title}</h1>
+    </div>
+   );
+}
+
+function AddTask({addedTodo}){ 
+  const [task, setTask] = useState("");
+
+  const handleAddTask = (event) =>{
+    const taskValue = event.target.value;
+    setTask((_) => taskValue)
+  }
+
+  const addingTask = () => {
+    addedTodo(task); 
+  }
+
+  return(
+  <div>
+    <input placeholder="Add new task" onChange={handleAddTask} value={task}/>
+    <button onClick={addingTask}>+</button>
+  </div>
+
+  );
+}
+
+function ShowTask({listOfTask}){
+  return(
+    <div className='todo-ShowTask'>
+      <ol>
+      {
+        listOfTask.map((todoelement, index) =>(
+          <>
+          <li key={index}>{todoelement}</li>
+          </>
+        )) 
+      }<p>a</p>
+      </ol>
+    </div>
+   
+  );
+}
+    
+function ClearTask({cleartasks}){
+  const clearalltask = () =>{
+    cleartasks();
+  } 
+  return(
+  <div>
+    <button onClick={clearalltask}>Clear All</button>
+  </div>
+  );
+}
+   
+
 
 export default App;
